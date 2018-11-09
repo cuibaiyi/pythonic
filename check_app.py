734b5784fed3dfb03a9e73fca1a067ea52807ebf
cuibaiyi <619876714@qq.com>
2018-11-09 23:33:01 +0800

    v2
    小更新

--- check_app.py
+++ check_app.py
@@ -5,21 +5,21 @@ import json
 
 class Check():
     def __init__(self):
-        self.url = 'http://118.89.217.80:9093/api/v1/alerts'
-        self.token = '3ee20aba6c55414c77e8a05477b75cacd12808751f5c10924e719c883eba7bad'
+        self.url = 'http://ip:9093/api/v1/alerts'
+        self.token = '3ee20aba6c55414c77xxxxxxxxxxxx'
         self.ding = 'https://oapi.dingtalk.com/robot/send?access_token=' + self.token
         self.headers = {"Content-Type": "application/json"}
 
+    #获取报警数据   
     def Get(self):
         ret = requests.get(self.url)
         data = ret.text
         json_data = json.loads(data)
         li = []
-        Sum = 1#len(json_data['data'])
+        Sum = len(json_data['data'])
         for n in range(Sum):
             d = json_data['data'][n]
             data = {}
-            #d = json_data['data'][0]
             data['check_name'] = d['labels']['alertname']
             data['job_name'] = d['labels']['job']
             data['demo'] = d['annotations']['summary']
@@ -29,16 +29,17 @@ class Check():
             li.append(data)
         return (li,Sum) if Sum else None
 
+    #发送钉钉报警
     def Post(self):
         for i in range(self.Get()[1]):
             d = self.Get()[0]
             payload = {
                 "msgtype": "markdown",
                 "markdown": {"title":"{job_name}报警".format(job_name=d[i]['job_name']),
-                "text": "#### **报警信息: {check_name}** \n\n 1.报警提示: {demo} \n\n 2.[监控详情链接]({url}) \n\n @18610160109 请查看".format(url=d[i]['url'], demo=d[i]['demo'], check_name=d[i]['check_name'])
+                "text": "#### **报警信息: {check_name}** \n\n 1.报警提示: {demo} \n\n 2.[监控详情链接]({url}) \n\n @186xxx 请查看".format(url=d[i]['url'], demo=d[i]['demo'], check_name=d[i]['check_name'])
                  },
                 "at": {
-                "atMobiles": ["18610160109"],
+                "atMobiles": ["186xxx"],
                 "isAtAll": True #False
                  }
             }
@@ -49,6 +50,7 @@ class Check():
             print(data)
         return True
 
+    
 if '__main__' == __name__:
     obj = Check()
     if obj.Get():
