07b3dd4d47b3619838991608e07ec614ad1c1953
carlos <619876714@qq.com>
2018-11-14 15:26:52 +0800

    Update diff_file.py

--- diff_file.py
+++ diff_file.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
-```
-对比2个文件的差异，并生成html页面展示
-```
+#对比2个文件的差异，并生成html页面展示
+
 import difflib
 hd = difflib.HtmlDiff()
 
 mems = loads = ''
 
-with open('/etc/passwd','r') as load:
-    loads = load.readlines()
+with open('/etc/passwd','r') as f:
+    loads = f.readlines()
 
-with open('/tmp/passwd', 'r') as mem:
-    mems = mem.readlines()
+with open('/tmp/passwd', 'r') as f:
+    mems = f.readlines()
 
-with open('diff.html','a+') as fo:
-    fo.write(hd.make_file(loads,mems))
+with open('diff.html','a+') as f:
+    f.write(hd.make_file(loads,mems))
